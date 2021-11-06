868c14aae4cdf5fb673723d0a5cd960174219766
Santhosh2139 <93145985+Santhosh2139@users.noreply.github.com>
2021-11-05 23:40:10 -0700

    Add files via upload

---
+++ athemetic 3.c
@@ -0,0 +1,28 @@
+//printing all arithmetic operations
+#include<stdio.h>
+void main()
+{
+	int a, b, s, d, p, q, r;
+	a = 50;
+	b = 35;
+	
+	s=a+b;
+	
+	printf("sum of %d and %d is %d", a, b, s);
+	
+	d=a-b;
+	
+	printf("\nsum of %d and %d is %d", a, b, d);
+	
+	q=a/b;
+	
+	printf("\nsum of %d and %d is %d", a, b, q);
+	
+	p=a*b;
+	
+	printf("\n\t\t\t\t\t sum of %d and %d is %d", a, b, p);
+		
+	r=a%b;
+		
+	printf("\nsum of %d and %d is %d", a, b, r);
+}
